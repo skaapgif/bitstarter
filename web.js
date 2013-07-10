b2503e6588c93764a4a872202cf2f24fd7da489b
Rudolf Meijering <skaapgif@gmail.com>
2013-07-10 22:51:45 +0200

    Upgrade node, serve index.html file from '/' url

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var data = fs.readFileSync('./index.html').toString();
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
